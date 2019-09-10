659e8362a37aeb0620685bca37d7ebaa194cf8bc
AnanJaser <ananjaser@gmail.com>
2019-09-10 11:15:08 +0400

    trelte-common: Set SDK API level for rild
    * P made changes to pthread_mutex_init() that are incompatible
      with our libsec-ril libraries. In order for rild to even see a
      radio as being availble we need the legacy behavior.
    * Even though our libsec-ril libraries are from 7.0, which is API
      level 24, we do not appear to require any of the other behaviors
      from API levels earlier than 27.
    
    Co-Authored-By: Aim Hi <nx111@users.noreply.github.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,6 +30,10 @@ SF_PRIMARY_DISPLAY_ORIENTATION := 270
 # Kernel
 TARGET_KERNEL_CONFIG := gts210velte_defconfig
 
+# Legacy BLOB Support
+TARGET_PROCESS_SDK_VERSION_OVERRIDE += \
+    /system/vendor/bin/hw/rild=27
+
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 
